5427a16e88cfc1a76b0b3fc1f1337828682c20d7
Nayan.Dahare <nayandahare02@gmail.com>
2025-12-13 20:00:33 +0530

    DB commit

---
+++ STUDENTS_DB/src/utils/DBUtils.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+
+	private static Connection con;
+	
+	public static Connection getConnection() throws SQLException
+	{
+		if(con==null)
+		{
+			con =DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/student_db1312", "root", "gojo"
+					);
+		}
+		return con; // return is manadetory to prevent error
+	}
+}
