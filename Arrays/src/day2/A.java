ff075803e7d932a05e3708e817fe38956e643346
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-20 16:36:41 +0530

    arrays rewind started d2 commit

---
+++ Arrays/src/day2/A.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class A {
+
+	public static void main(String[] args) {
+		int a[] = new int[3];
+		a[0] =10;
+		a[1] =20;
+		a[2] =30;
+		for(int i = 0; i < 3; i++)
+		{
+			System.out.println(a[i] + " ");
+		}
+		
+		for(int b:a)
+		{
+			System.out.print(b+ " ");
+		}
+
+	}
+
+}
