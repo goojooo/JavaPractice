0135ab6a3c13040b154241307461cc384d3ca51c
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-21 23:07:45 +0530

    arrays rewind started d3 commit

--- Arrays/src/day3/I.java
+++ Arrays/src/day3/I.java
@@ -3,8 +3,23 @@ package day3;
 public class I {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Smallest element
+		
+		int a[] = {35,643,4,54,67,854,67,82};
+		int min = 0;
+		System.out.println("Array : ");
+		for(int i= 0 ; i < a.length;i++)
+		{
+			System.out.print(a[i] + " ");
+		}
+		min=a[0];
+		for(int i = 1 ; i <a.length ; i++)
+		{
+			if(min> a[i]){
+				min = a[i];
+			}
+		}
+		System.out.println("\nSmallest elemet : " + min);
 	}
 
 }
