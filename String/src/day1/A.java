3dc86f0e8cff22c48118ff0970345e6769a6d888
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-23 15:58:29 +0530

    String started d1 commit

---
+++ String/src/day1/A.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class A {
+
+	public static void main(String[] args) {
+		// String is non-primitive data type
+		// String is the sequence of charecter(Array of charecters)
+		// CharSequence is interface 
+		// String is a class 
+		// String is immutable  object 
+		String name = "Nayan";
+		name = "dahare";
+		System.out.println(name);
+	}
+
+}
