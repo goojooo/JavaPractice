29624ee6cb81c4ec8b8faccada12c3c775a693ef
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-23 16:05:47 +0530

    String started d1 commit

---
+++ String/src/day1/StringConstantPool.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class StringConstantPool {
+
+	public static void main(String[] args) {
+		// String Constant Pool - present in HEAP area
+		// 
+		String s1 = new String("Nayan");
+		s1 = "wew";
+		System.out.println(s1);
+	}
+
+}
