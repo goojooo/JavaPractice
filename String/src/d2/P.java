c1c3c5d1398fe7c6d731d5abf973b1d8394cdbab
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-28 22:06:30 +0530

    pp commit

--- String/src/d2/P.java
+++ String/src/d2/P.java
@@ -20,14 +20,14 @@ public class P {
 //			}
 //			System.out.println();
 //		}
-		for(int i = 1 ; i <= 3 ; i++)
-		{
-			for(int j = 3 ; i <= j ; j--)
-			{
-				System.out.print(i + " ");
-			}
-			System.out.println();
-		}
+//		for(int i = 1 ; i <= 3 ; i++)
+//		{
+//			for(int j = 3 ; i <= j ; j--)
+//			{
+//				System.out.print(i + " ");
+//			}
+//			System.out.println();
+//		}
 		System.out.println("Checked!");
 //		for(int i = 1 ; i <= 5 ; i++)
 //		{
@@ -39,14 +39,20 @@ public class P {
 //			}
 //			System.out.println();
 //		}
-		for(int i = 1 ; i <=3 ; i++)
+//		for(int i = 1 ; i <=3 ; i++)
+//		{
+//			for(int j = 1 ; j<=i ; j++)
+//			{
+//				System.out.print(j + " ");
+//			}
+//			System.out.println();
+//		}
+		String a = "abcd";
+		for(int i = a.length()-1 ; i >= 0 ; i--)
 		{
-			for(int j = 1 ; j>=i ; j++)
-			{
-				System.out.print(j + " ");
-			}
-			System.out.println();
+			System.out.print(a.charAt(i));
 		}
 	}
+	
 
 }
