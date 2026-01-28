a47aebedc2ecacf41733957720b7a1136daf3eea
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-28 23:34:43 +0530

    pp commit

---
+++ fitnessWorld/src/test/java/com/fitnessWorld/fitnessWorld/FitnessWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessWorld.fitnessWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
