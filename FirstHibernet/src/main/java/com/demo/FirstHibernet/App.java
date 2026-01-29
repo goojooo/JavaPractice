db16f9104a3868fcddec1a9c4f0abcc65f4788b4
Nayan.Dahare <nayandahare02@gmail.com>
2026-01-29 19:14:50 +0530

    pp commit

--- FirstHibernet/src/main/java/com/demo/FirstHibernet/App.java
+++ FirstHibernet/src/main/java/com/demo/FirstHibernet/App.java
@@ -1,5 +1,7 @@
 package com.demo.FirstHibernet;
 
+import java.lang.module.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World...!" );
+        System.out.println( "Project Started...!" );
+        Configuration cfg = new Configuration();
+//        SessionFactory fact = new 
     }
 }
